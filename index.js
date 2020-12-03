050b2568c7086e9ca2444139a7158bf056bdbad3
b0g4r4 <b0g4r4@gmail.com>
2020-12-03 07:51:25 +0700

    cabang-1, selamat pagi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.get('/', (req, res)=>{
     console.log('an request happen in route \'/\'')
-    res.send('Hello World, From Rifki. Sekarang '+ moment().format('D MMMM YYYY, h:mm:ss a'))
+    res.send('Selamat pagi, From Rifki. Sekarang '+ moment().format('D MMMM YYYY, h:mm:ss a'))
 })
 
-app.listen(port, () => console.log(`Hello World app listening on port ${port}!`))
+app.listen(port, () => console.log(`Selamat pagi app listening on port ${port}!`))
